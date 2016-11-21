21a5a693b185476d6a5eb80fdee630581cac9434
Manu <manuel.maute92@gmail.com>
2016-11-21 15:42:13 +0100

    initial commit

---
+++ src/cheat.js
@@ -0,0 +1,17 @@
+/*
+
+import { EventEmitter } from 'events'
+import dispatcher from '../dispatcher'
+import axios from 'axios'
+import { Dispatcher } from 'flux'
+
+
+const store = new Store
+dispatcher.register(store.handleActions.bind(store))
+
+// Make this available on the browser console
+window.dispatcher = dispatcher
+
+export default store
+
+ */
--- src/js/pages/Layout.js
+++ src/js/pages/Layout.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import HeaderComponent from '../components/HeaderComponent'
 import ContentComponent from '../components/ContentComponent'
+import TestComponent from '../components/TestComponent'
 
 export default class Layout extends React.Component {
   constructor() {
@@ -9,8 +10,7 @@ export default class Layout extends React.Component {
   render() {
     return (
       <div>
-        <HeaderComponent/>
-        <ContentComponent/>
+        <TestComponent/>
       </div>
     )
   }
